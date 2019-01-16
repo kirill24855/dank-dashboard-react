e4619f4e018f6f46fd2106b33b7d71f2b37345df
kirill24855 <kirill.shpin@gmail.com>
2019-01-16 11:55:50 -0800

    Updates

---
+++ precache-manifest.1f226bf8ee96f13545e009d3d33dffa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fccdf1f56dba11ee6367",
+    "url": "/dank-dashboard-react/static/js/runtime~main.fccdf1f5.js"
+  },
+  {
+    "revision": "32837dbbb035d7e4ac15",
+    "url": "/dank-dashboard-react/static/js/main.32837dbb.chunk.js"
+  },
+  {
+    "revision": "9eb65825040bcecc9e9a",
+    "url": "/dank-dashboard-react/static/js/1.9eb65825.chunk.js"
+  },
+  {
+    "revision": "32837dbbb035d7e4ac15",
+    "url": "/dank-dashboard-react/static/css/main.76639b0b.chunk.css"
+  },
+  {
+    "revision": "21147f98e6c22b305f6d4898a80ca569",
+    "url": "/dank-dashboard-react/index.html"
+  }
+];
