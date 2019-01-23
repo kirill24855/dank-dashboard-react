4af0a51b155a6acc71a544b01787489f58ae588c
kirill24855 <kirill.shpin@gmail.com>
2019-01-22 22:29:25 -0800

    Updates

--- precache-manifest.c8cbe2931b75425e5e8de95e35a27893.js
+++ precache-manifest.c8cbe2931b75425e5e8de95e35a27893.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dank-dashboard-react/static/js/runtime~main.fccdf1f5.js"
   },
   {
-    "revision": "cdd0b2977d5329e18493",
-    "url": "/dank-dashboard-react/static/js/main.cdd0b297.chunk.js"
+    "revision": "2ef404d350c291c5eac6",
+    "url": "/dank-dashboard-react/static/js/main.2ef404d3.chunk.js"
   },
   {
     "revision": "a20579c859cf4cd8adee",
     "url": "/dank-dashboard-react/static/js/1.a20579c8.chunk.js"
   },
   {
-    "revision": "cdd0b2977d5329e18493",
+    "revision": "2ef404d350c291c5eac6",
     "url": "/dank-dashboard-react/static/css/main.577574a5.chunk.css"
   },
   {
-    "revision": "f7696edc8bd8ceff568946ca3618de7d",
+    "revision": "83f3fb9b4f8f9f40e4613cf7df055b64",
     "url": "/dank-dashboard-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/dank-dashboard-react/precache-manifest.7e13b8f4c1d455390cb611cea4a2a524.js"
+  "/dank-dashboard-react/precache-manifest.c8cbe2931b75425e5e8de95e35a27893.js"
 );
 
 workbox.clientsClaim();
